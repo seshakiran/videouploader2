be2e50a94cbe9d305a66bf9b18e8b319cc76a24d
seshakiran <kiranadimatyam@rocketmail.com>
2020-10-08 20:32:41 -0500

    5th commit

--- videouploader2.py
+++ videouploader2.py
@@ -1,5 +1,18 @@
+import boto3
+s3 = boto3.client("s3")
+
 def handler(event, context):
     evtHeaders = event.headers
     fileContent = event.Body
     fullFileName = evtHeaders[0]
-    return ("event headers are ", evtHeaders[0])
+    try:
+        data = s3.put_object(
+            Bucket="movilti-user-reviews",
+            Key=fullFileName,
+            Body=fileContent,
+            Metadata={}
+        )
+    except BaseException as e:
+        print(e)
+        raise(e)
+    return ("event headers are ", fullFileName)
